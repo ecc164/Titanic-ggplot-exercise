e4e4083ddb3299481e079e482537bb0a74842c9a
ecc164 <50721814+ecc164@users.noreply.github.com>
2019-06-17 16:36:28 -0700

    Add files via upload

---
+++ Titanic.R
@@ -0,0 +1,13 @@
+# 1 - Check the structure of titanic
+str(titanic)
+
+# 2 - Use ggplot() for the first instruction
+ggplot(titanic, aes(x = Pclass, fill = Sex)) +
+  geom_bar(position = "dodge")
+
+# 3 - Plot 2, add facet_grid() layer
+ggplot(titanic, aes(x = Pclass, fill = Sex)) +
+  geom_bar(position = "dodge") + facet_grid(. ~ Survived)
+
+# 4 - Define an object for position jitterdodge, to use below
+posn.jd <- position_jitterdodge(0.5, 0, 0.6)
